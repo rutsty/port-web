b2b8679f4ef3257779a983c6548c861909881e4f
yan <167824432+rutsty@users.noreply.github.com>
2026-03-22 13:18:11 -0300

    Update get-lattice.js

--- api/get-lattice.js
+++ api/get-lattice.js
@@ -1,10 +1,11 @@
 export default async function handler(req, res) {
-    
     const token = process.env.AIRTABLE_TOKEN;
     const baseId = 'appondFVTjAbYGUxP'; 
-    const tableId = 'Bitacora';         
 
-    const url = `https://api.airtable.com/v0/${baseId}/${tableId}?sort[0][field]=fecha&sort[0][direction]=desc`;
+    const tableId = 'tbl51JlRNS7Idj67j'; 
+
+    
+    const url = `https://api.airtable.com/v0/${baseId}/${tableId}?sort[0][field]=fecha%20y%20hora&sort[0][direction]=desc`;
 
     try {
         const response = await fetch(url, {
@@ -12,9 +13,13 @@ export default async function handler(req, res) {
         });
         const data = await response.json();
         
- 
-        res.status(200).json(data);
+        if (data.error) {
+            return res.status(400).json({ error: data.error });
+        }
+
+        // Enviamos los registros puros para que la web los maneje
+        res.status(200).json(data.records);
     } catch (error) {
-        res.status(500).json({ error: "Fallo de conexión con Airtable" });
+        res.status(500).json({ error: "Fallo de conexión" });
     }
 }
